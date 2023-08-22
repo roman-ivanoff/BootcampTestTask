c3859e2b35a855cb7723d1fd28d231a460c8df93
Roman Ivanov <roman.ivanoff124@gmail.com>
2023-08-22 19:42:03 +0300

    added models

---
+++ BootcampTestTask/Models/Dto/EquipmentDto.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+struct EquipmentDto: Decodable {
+    let date: String
+    let day: Int
+    let aircraft: Int
+    let helicopter: Int
+    let tank: Int
+    let apc: Int
+    let fieldArtillery: Int
+    let mrl: Int
+    let militaryAuto: Int?
+    let fuelTank: Int?
+    let drone: Int
+    let navalShip: Int
+    let antiAircraftWarfare: Int
+    let specialEquipment: Int?
+    let mobileSRBMSystem: Int
+    let greatestLossesDirection: String?
+    let vehiclesAndFuelTanks: Int?
+    let cruiseMissiles: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case date
+        case day
+        case aircraft
+        case helicopter
+        case tank
+        case apc = "APC"
+        case fieldArtillery = "field artillery"
+        case mrl = "MRL"
+        case militaryAuto = "military auto"
+        case fuelTank = "fuel tank"
+        case drone
+        case navalShip = "naval ship"
+        case antiAircraftWarfare = "anti-aircraft warfare"
+        case specialEquipment = "special equipment"
+        case mobileSRBMSystem = "mobile SRBM system"
+        case greatestLossesDirection = "greatest losses direction"
+        case vehiclesAndFuelTanks = "vehicles and fuel tanks"
+        case cruiseMissiles = "cruise missiles"
+    }
+}
